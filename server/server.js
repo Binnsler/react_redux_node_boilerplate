ead4ebac0d35c22c8475b5f56b8667a1dad853d4
marczenn <jamie.m.binns@gmail.com>
2016-09-06 18:27:36 -0600

    sometimes I hate myself

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ var app = express();
 var http = require('http');
 var path = require('path');
 
-app.get('*', function(req, res){
-  res.sendFile(path.resolve('./index.html'));
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('../index.html'));
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3030;
 const server = http.createServer(app);
 server.listen(port, function(){
   console.log('Listening on porto: ', port);
